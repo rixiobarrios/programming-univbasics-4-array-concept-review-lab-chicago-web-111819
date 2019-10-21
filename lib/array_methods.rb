791a764d098f21e6483b21d241fb2629051628cc
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-21 21:48:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,8 +6,10 @@ def find_element_index(array, value_to_find)
   while count < array.length do
     if array[count] == value_to_find
       return count
+    end
+    count += 1
   end
-  count += 1
+  nil
 end
 
 def find_max_value(array)
