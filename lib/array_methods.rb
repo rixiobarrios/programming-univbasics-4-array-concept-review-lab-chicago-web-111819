03f7a7f5c9376b19e416d5ff4aa4c9b822b4a30c
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-21 22:12:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,5 +23,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  x = 0
+  array.length.times do |index|
+    if array[index] < x
+       x = array[index]
+    end
+  end
+  x
+end
 end
