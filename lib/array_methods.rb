1ab7f5614e28e55b7c6d61c357281c36a42bd250
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-21 22:18:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,12 +22,17 @@ def find_max_value(array)
   x
 end
 
+# def find_min_value(array)
+#   x = 0
+#   array.length.times do |index|
+#     if array[index] < x
+#        x = array[index]
+#     end
+#   end
+#   x
+# end
 def find_min_value(array)
   x = 0
-  array.length.times do |index|
-    if array[index] < x
-       x = array[index]
-    end
-  end
-  x
+  array.length.times { |index| x = array[index] if array[index] > x }
+  x 
 end
